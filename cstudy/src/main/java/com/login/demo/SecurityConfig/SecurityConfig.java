4bd40398a0a3e66d0ecfe2a5eb13a18bbc7c24c2
hwangjaeyong <hwangjaeyong@172.30.1.56>
2021-07-17 16:21:24 +0900

    (#0001)application.properties, securityConfig

--- cstudy/src/main/java/com/login/demo/SecurityConfig/SecurityConfig.java
+++ cstudy/src/main/java/com/login/demo/SecurityConfig/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.login.demo.SecurityConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -49,6 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		       .and()
 		       .withUser("admin").password("password").roles("ADMIN");
 	}
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
